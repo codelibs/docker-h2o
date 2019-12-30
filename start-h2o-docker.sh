3cde3810a9af6afa862cddeea0a1807ce921e0f1
Shinsuke Sugaya <shinsuke@apache.org>
2019-12-30 22:37:50 +0900

    add H2O_OPTIONS

--- start-h2o-docker.sh
+++ start-h2o-docker.sh
@@ -24,20 +24,5 @@ java -version
 # Check that we can at least run H2O with the given java.
 java -jar ${H2O_HOME}/h2o.jar -version
 
-# HDFS credentials.
-hdfs_config_option=""
-hdfs_config_value=""
-hdfs_option=""
-hdfs_option_value=""
-hdfs_version=""
-if [ -f .ec2/core-site.xml ]
-then
-  hdfs_config_option="-hdfs_config"
-  hdfs_config_value=".ec2/core-site.xml"
-  hdfs_option="-hdfs"
-  hdfs_option_value=""
-  hdfs_version=""
-fi
-
 # Start H2O
-java -Xmx${H2O_MAX_HEAP} -jar ${H2O_HOME}/h2o.jar -name ${H2O_NAME} -flatfile flatfile.txt -port 54321 ${hdfs_config_option} ${hdfs_config_value} ${hdfs_option} ${hdfs_option_value} ${hdfs_version}
+java ${H2O_JVM_OPTIONS} -Xmx${H2O_MAX_HEAP} -jar ${H2O_HOME}/h2o.jar -name ${H2O_NAME} -flatfile flatfile.txt -port 54321 ${H2O_OPTIONS}
