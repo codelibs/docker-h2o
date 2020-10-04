45f8155e4b9bb183f8d42d4f85ced45d1c3af2f3
Shinsuke Sugaya <shinsuke@apache.org>
2020-10-04 22:46:24 +0900

    remove version info

--- start-h2o-docker.sh
+++ start-h2o-docker.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-d=`dirname $0`
-
 if [[ "x${H2O_NAME}" = "x" ]] ; then
   H2O_NAME=H2OServer
 fi
@@ -16,13 +14,6 @@ if [[ "x${H2O_MAX_HEAP}" = "x" ]] ; then
   xmxMb=$[ $tmp / 100 ]
   H2O_MAX_HEAP=${xmxMb}m
 fi
-
-# First try running java.
-
-java -version
-
-# Check that we can at least run H2O with the given java.
-java -jar ${H2O_HOME}/h2o.jar -version
 
 # Start H2O
 java ${H2O_JVM_OPTIONS} -Xmx${H2O_MAX_HEAP} -jar ${H2O_HOME}/h2o.jar -name ${H2O_NAME} -flatfile flatfile.txt -port 54321 ${H2O_OPTIONS}
